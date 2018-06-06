79af7a2ae4c68b8939e2aad6e60fa7f1bb7d606f
toversus <toversus2357@gmail.com>
2018-06-06 21:50:39 +0900

    add helper function to print readable layer of tests

--- ast/ast.go
+++ ast/ast.go
@@ -1,11 +1,19 @@
 package ast
 
-import "github.com/toversus/monkey/token"
+import (
+	"bytes"
+
+	"github.com/toversus/monkey/token"
+)
 
 type Node interface {
 	// TokenLiteral returns literal value of the associated token.
 	// It is used only for debugging and testing.
 	TokenLiteral() string
+
+	// String prints AST nodes for debugging ans compares them with other AST nodes,
+	// and it is handy in tests.
+	String() string
 }
 
 // Statement doesn't produce a value.
@@ -37,7 +45,18 @@ func (p *Program) TokenLiteral() string {
 	return ""
 }
 
-//
+// String returns whole program back as a string for readable tests.
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
+// LetStatement ...
 type LetStatement struct {
 	// Token is the token.LET token.
 	Token token.Token
@@ -51,6 +70,22 @@ type LetStatement struct {
 
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
 
 // Identifier is used to hold the identifier of the binding.
 // It is not Expression when it binds a variable, e.g. let x = 5;
@@ -66,6 +101,8 @@ type Identifier struct {
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 
+func (i *Identifier) String() string { return i.Value }
+
 // ReturnStatement consists of solely of the keyword "return" and an "expression".
 type ReturnStatement struct {
 	Token token.Token
@@ -76,3 +113,36 @@ type ReturnStatement struct {
 
 func (rs *ReturnStatement) statementNode()       {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+// ExpressionStatement is wrapper for adding it to the Statements slice of ast.Program
+// to reuse work on the parser.
+type ExpressionStatement struct {
+	// The first token of the expression.
+	Token token.Token
+
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
