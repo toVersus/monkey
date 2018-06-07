5ddf5e285b04f1c48b1973127e4c86486ea70f0f
toversus <toversus2357@gmail.com>
2018-06-07 22:18:39 +0900

    add parser for identifier expression

--- parser/parser.go
+++ parser/parser.go
@@ -8,6 +8,20 @@ import (
 	"github.com/toversus/monkey/token"
 )
 
+const (
+	// To assign increment numbers starting from 1 as the constant values.
+	_ int = iota
+
+	// The order and the relation to each other are critical for representing precedence.
+	LOWEST
+	EQUALS      // ==
+	LESSGREATER // > or <
+	SUM         // +
+	PRODUCT     // *
+	PREFIX      // -X or !X
+	CALL        //myFunction(X)
+)
+
 // Parser is used to construct AST.
 type Parser struct {
 	l *lexer.Lexer
@@ -18,7 +32,18 @@ type Parser struct {
 	curToken token.Token
 	// peekToken is also "pointers" to the next token.
 	peekToken token.Token
+
+	// These maps can be used to get the correct parser function for the current token type.
+	prefixParseFns map[token.TokenType]prefixParseFn
+	infixParseFns  map[token.TokenType]infixParseFn
 }
+
+type (
+	prefixParseFn func() ast.Expression
+
+	// argument represents left side of the infix operator to be parsed.
+	infixParseFn func(ast.Expression) ast.Expression
+)
 
 // New initiates parser.
 func New(l *lexer.Lexer) *Parser {
@@ -27,11 +52,20 @@ func New(l *lexer.Lexer) *Parser {
 		errors: []string{},
 	}
 
+	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
+	p.registerPrefix(token.IDENT, p.parseIdentifier)
+
 	// Read two tokens, so curToken and peekToken are both set.
 	p.nextToken()
 	p.nextToken()
 
 	return p
+}
+
+// parseIdentifier just returns token and its value.
+// It doesn't advance the tokens by calling nextToken.
+func (p *Parser) parseIdentifier() ast.Expression {
+	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
 }
 
 // nextToken is a helper function to advance both curToken and peekToken.
@@ -63,8 +97,13 @@ func (p *Parser) parseStatement() ast.Statement {
 		return p.parseLetStatement()
 	case token.RETURN:
 		return p.parseReturnStatement()
-	default:
+
+	// Workaround for passing TestLetStatements at this time.
+	case token.SEMICOLON:
 		return nil
+
+	default:
+		return p.parseExpressionStatement()
 	}
 }
 
@@ -137,4 +176,37 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 	}
 
 	return stmt
+}
+
+// registerPrefix registers prefix parsing function as associated token type.
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
+	p.prefixParseFns[tokenType] = fn
+}
+
+// registerInfix registers infix parsing function as associated token type.
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
+	p.infixParseFns[tokenType] = fn
+}
+
+func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
+	stmt := &ast.ExpressionStatement{Token: p.curToken}
+
+	stmt.Expression = p.parseExpression(LOWEST)
+
+	// Semicolon is optional in this context.
+	if p.peekTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseExpression(precedence int) ast.Expression {
+	prefix := p.prefixParseFns[p.curToken.Type]
+	if prefix == nil {
+		return nil
+	}
+	leftExp := prefix()
+
+	return leftExp
 }
