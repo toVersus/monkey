0420d05322ddf5c60081568422f7b1f47081bac0
toversus <toversus2357@gmail.com>
2018-06-17 11:12:46 +0900

    implement object system for evaluating integer, boolean and null types

---
+++ object/object.go
@@ -0,0 +1,39 @@
+// Package object is used to evaluate AST node using tree-waliking evaluator on the fly.
+package object
+
+import "fmt"
+
+type ObjectType string
+
+const (
+	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ    = "NULL"
+)
+
+// Object is implemented as interface because every value needs a diffrent internal representation
+// and it's easier to define two different struct types than fitting basic data types into the same struct fields.
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
+
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
+
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
+
+// Null has no value wrapped in host language.
+type Null struct{}
+
+func (n *Null) Type() ObjectType { return NULL_OBJ }
+func (n *Null) Inspect() string  { return "null" }
