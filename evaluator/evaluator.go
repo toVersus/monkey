39b1eff81c6c8d759e0dd07c3da810b2a535179f
toversus <toversus2357@gmail.com>
2018-06-27 18:52:10 +0900

    support len for array literals

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -22,8 +22,12 @@ var builtins = map[string]*object.Builtin{
 			}
 
 			switch arg := args[0].(type) {
+			case *object.Array:
+				return &object.Integer{Value: int64(len(arg.Elements))}
+
 			case *object.String:
 				return &object.Integer{Value: int64(len(arg.Value))}
+
 			default:
 				return newError("argument to 'len' not supported, got %s",
 					args[0].Type())
