870a48f96404dbb49fd5e54bf0be1874cdbc5df2
toversus <toversus2357@gmail.com>
2018-06-30 14:38:38 +0900

    fix error message on test for integer literal

--- parser/parse_test.go
+++ parser/parse_test.go
@@ -180,8 +180,8 @@ func TestIntegerLiteralExpression(t *testing.T) {
 			stmt.Expression)
 	}
 	if literal.Value != 5 {
-		t.Errorf("literal.TokenLiteral not %s. got=%s", "5",
-			literal.TokenLiteral())
+		t.Errorf("literal.Value not %d. got=%d", 5,
+			literal.Value)
 	}
 	if literal.TokenLiteral() != "5" {
 		t.Errorf("literal.TokenLiteral not %s. got=%s", "5",
