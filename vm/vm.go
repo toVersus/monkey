45a4940dd25c7b29d83ae13e422a38a98a78971e
toversus <toversus2357@gmail.com>
2018-08-07 23:37:25 +0900

    introduce vm to execute the bytecode

---
+++ vm/vm.go
@@ -0,0 +1,69 @@
+package vm
+
+import (
+	"fmt"
+
+	"github.com/toversus/monkey/code"
+	"github.com/toversus/monkey/compiler"
+	"github.com/toversus/monkey/object"
+)
+
+// StackSize is size of the stack.
+const StackSize = 2048
+
+type VM struct {
+	constants    []object.Object
+	instructions code.Instructions
+
+	stack []object.Object
+	sp    int // always points to the next value. Top of stack is stack[sp-1]
+}
+
+func New(bytecode *compiler.Bytecode) *VM {
+	return &VM{
+		instructions: bytecode.Instructions,
+		constants:    bytecode.Constants,
+
+		stack: make([]object.Object, StackSize),
+		sp:    0,
+	}
+}
+
+// StackTop returns object pushed onto the top of the stack.
+func (vm *VM) StackTop() object.Object {
+	if vm.sp == 0 {
+		return nil
+	}
+	return vm.stack[vm.sp-1]
+}
+
+func (vm *VM) Run() error {
+	for ip := 0; ip < len(vm.instructions); ip++ {
+		op := code.Opcode(vm.instructions[ip])
+
+		switch op {
+		case code.OpConstant:
+			constIndex := code.ReadUint16(vm.instructions[ip+1:])
+
+			ip += 2
+
+			err := vm.push(vm.constants[constIndex])
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func (vm *VM) push(o object.Object) error {
+	if vm.sp >= StackSize {
+		return fmt.Errorf("stack overflow")
+	}
+
+	vm.stack[vm.sp] = o
+	vm.sp++
+
+	return nil
+}
---
+++ vm/vm_test.go
@@ -0,0 +1,83 @@
+package vm
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/toversus/monkey/ast"
+	"github.com/toversus/monkey/compiler"
+	"github.com/toversus/monkey/lexer"
+	"github.com/toversus/monkey/object"
+	"github.com/toversus/monkey/parser"
+)
+
+func TestIntegerArithmetic(t *testing.T) {
+	tests := []vmTestCase{
+		{"1", 1},
+		{"2", 2},
+		{"1 + 2", 2}, // FIXME
+	}
+
+	runVmTests(t, tests)
+}
+
+type vmTestCase struct {
+	input    string
+	expected interface{}
+}
+
+func runVmTests(t *testing.T, tests []vmTestCase) {
+	t.Helper()
+
+	for _, test := range tests {
+		program := parse(test.input)
+
+		comp := compiler.New()
+		err := comp.Compile(program)
+		if err != nil {
+			t.Fatalf("compiler error: %s", err)
+		}
+
+		vm := New(comp.Bytecode())
+		err = vm.Run()
+		if err != nil {
+			t.Fatalf("vm error: %s", err)
+		}
+
+		stackElem := vm.StackTop()
+
+		testExpectedObject(t, test.expected, stackElem)
+	}
+}
+
+func testExpectedObject(t *testing.T, expected interface{}, actual object.Object) {
+	t.Helper()
+
+	switch expected := expected.(type) {
+	case int:
+		err := testIntegerObject(int64(expected), actual)
+
+		if err != nil {
+			t.Errorf("testIntegerObject failed: %s", err)
+		}
+	}
+}
+
+func parse(input string) *ast.Program {
+	l := lexer.New(input)
+	p := parser.New(l)
+	return p.ParseProgram()
+}
+
+func testIntegerObject(expected int64, actual object.Object) error {
+	result, ok := actual.(*object.Integer)
+	if !ok {
+		return fmt.Errorf("object is not Integer. got=%T (%+v)",
+			actual, actual)
+	}
+	if result.Value != expected {
+		return fmt.Errorf("object has wrong value. got=%d, want=%d",
+			result.Value, expected)
+	}
+	return nil
+}
