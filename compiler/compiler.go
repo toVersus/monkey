952c58798c1bedcc8095e0dd9329e45534d16fd2
toversus <toversus2357@gmail.com>
2018-08-20 20:51:23 +0900

    fix to be compiled with scopes

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -180,15 +180,24 @@ func (c *Compiler) Compile(node ast.Node) error {
 		if err := c.Compile(node.Value); err != nil {
 			return err
 		}
+
 		symbol := c.symbolTable.Define(node.Name.Value)
-		c.emit(code.OpSetGlobal, symbol.Index)
+		if symbol.Scope == GlobalScope {
+			c.emit(code.OpSetGlobal, symbol.Index)
+		} else {
+			c.emit(code.OpSetLocal, symbol.Index)
+		}
 
 	case *ast.Identifier:
 		symbol, ok := c.symbolTable.Resolve(node.Value)
 		if !ok {
 			return fmt.Errorf("undefined variable: %s", node.Value)
 		}
-		c.emit(code.OpGetGlobal, symbol.Index)
+		if symbol.Scope == GlobalScope {
+			c.emit(code.OpGetGlobal, symbol.Index)
+		} else {
+			c.emit(code.OpGetLocal, symbol.Index)
+		}
 
 	case *ast.StringLiteral:
 		str := &object.String{Value: node.Value}
@@ -372,6 +381,8 @@ func (c *Compiler) enterScope() {
 	}
 	c.scopes = append(c.scopes, scope)
 	c.scopeIndex++
+
+	c.symbolTable = NewEnclosedSymbolTable(c.symbolTable)
 }
 
 func (c *Compiler) leaveScope() code.Instructions {
@@ -379,6 +390,8 @@ func (c *Compiler) leaveScope() code.Instructions {
 
 	c.scopes = c.scopes[:len(c.scopes)-1]
 	c.scopeIndex--
+
+	c.symbolTable = c.symbolTable.Outer
 
 	return instructions
 }
--- compiler/compiler_test.go
+++ compiler/compiler_test.go
@@ -25,6 +25,7 @@ func TestCompilerScopes(t *testing.T) {
 	if compiler.scopeIndex != 0 {
 		t.Errorf("scopeIndex wrong. got=%d, want=%d", compiler.scopeIndex, 0)
 	}
+	globalSymbolTable := compiler.symbolTable
 
 	compiler.emit(code.OpMul)
 
@@ -34,6 +35,7 @@ func TestCompilerScopes(t *testing.T) {
 	}
 
 	compiler.emit(code.OpSub)
+
 	if len(compiler.scopes[compiler.scopeIndex].instructions) != 1 {
 		t.Errorf("instructions length wrong. got=%d",
 			len(compiler.scopes[compiler.scopeIndex].instructions))
@@ -45,13 +47,25 @@ func TestCompilerScopes(t *testing.T) {
 			last.Opcode, code.OpSub)
 	}
 
+	if compiler.symbolTable.Outer != globalSymbolTable {
+		t.Error("compiler did not restore global symbol table")
+	}
+
 	compiler.leaveScope()
 	if compiler.scopeIndex != 0 {
 		t.Errorf("scopeIndex wrong. got=%d, want=%d",
 			compiler.scopeIndex, 0)
 	}
 
+	if compiler.symbolTable != globalSymbolTable {
+		t.Error("compiler did not restore global symbol table")
+	}
+	if compiler.symbolTable.Outer != nil {
+		t.Error("compiler modified global symbol table incorrectly")
+	}
+
 	compiler.emit(code.OpAdd)
+
 	if len(compiler.scopes[compiler.scopeIndex].instructions) != 2 {
 		t.Errorf("instructions length wrong. got=%d, want=%d",
 			last.Opcode, code.OpAdd)
