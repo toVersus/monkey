57e68f3867603211df8d42e4ed9da23f12d8f53f
toversus <toversus2357@gmail.com>
2018-08-23 20:26:37 +0900

    move built-ins to object package

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,123 +1,14 @@
 package evaluator
 
 import (
-	"fmt"
-
 	"github.com/toversus/monkey/object"
 )
 
 var builtins = map[string]*object.Builtin{
-	"len":   &object.Builtin{Fn: lenFunc},
-	"first": &object.Builtin{Fn: firstFunc},
-	"last":  &object.Builtin{Fn: lastFunc},
-	"rest":  &object.Builtin{Fn: restFunc},
-	"push":  &object.Builtin{Fn: pushFunc},
-	"puts":  &object.Builtin{Fn: putsFunc},
-}
-
-func lenFunc(args ...object.Object) object.Object {
-	if len(args) != 1 {
-		return newError("wrong number of arguments. got=%d, want=1",
-			len(args))
-	}
-
-	switch arg := args[0].(type) {
-	case *object.Array:
-		return &object.Integer{Value: int64(len(arg.Elements))}
-
-	case *object.String:
-		return &object.Integer{Value: int64(len(arg.Value))}
-
-	default:
-		return newError("argument to 'len' not supported, got %s",
-			args[0].Type())
-	}
-}
-
-func firstFunc(args ...object.Object) object.Object {
-	if len(args) != 1 {
-		return newError("wrong number of arguments. got=%d, want=1",
-			len(args))
-	}
-	if args[0].Type() != object.ARRAY_OBJ {
-		return newError("argument to 'first' must be ARRAY, got %s",
-			args[0].Type())
-	}
-
-	arr := args[0].(*object.Array)
-	if len(arr.Elements) > 0 {
-		return arr.Elements[0]
-	}
-
-	return NULL
-}
-
-func lastFunc(args ...object.Object) object.Object {
-	if len(args) != 1 {
-		return newError("wrong number of arguments. got=%d, want=1",
-			len(args))
-	}
-	if args[0].Type() != object.ARRAY_OBJ {
-		return newError("argument to 'last' must be ARRAY, got %s",
-			args[0].Type())
-	}
-
-	arr := args[0].(*object.Array)
-	length := len(arr.Elements)
-	if length > 0 {
-		return arr.Elements[length-1]
-	}
-
-	return NULL
-}
-
-// restFunc returns a newly allocated array which contains all elements of the array passed as argument,
-// except the first one.
-func restFunc(args ...object.Object) object.Object {
-	if len(args) != 1 {
-		return newError("wrong number of arguments. got=%d, want=1",
-			len(args))
-	}
-	if args[0].Type() != object.ARRAY_OBJ {
-		return newError("argument to 'rest' must be ARRAY, got %s",
-			args[0].Type())
-	}
-
-	arr := args[0].(*object.Array)
-	length := len(arr.Elements)
-	if length > 0 {
-		newElements := make([]object.Object, length-1, length-1)
-		copy(newElements, arr.Elements[1:length])
-		return &object.Array{Elements: newElements}
-	}
-
-	return NULL
-}
-
-func pushFunc(args ...object.Object) object.Object {
-	if len(args) != 2 {
-		return newError("wrong number of arguments. got=%d, want=2",
-			len(args))
-	}
-	if args[0].Type() != object.ARRAY_OBJ {
-		return newError("argument to 'push' must be ARRAY, got %s",
-			args[0].Type())
-	}
-
-	arr := args[0].(*object.Array)
-	length := len(arr.Elements)
-	newElements := make([]object.Object, length+1, length+1)
-	copy(newElements, arr.Elements)
-	newElements[length] = args[1]
-
-	return &object.Array{Elements: newElements}
-}
-
-// putsFunc prints the given arguments on new lines to STDOUT.
-func putsFunc(args ...object.Object) object.Object {
-	for _, arg := range args {
-		fmt.Println(arg.Inspect())
-	}
-
-	return NULL
+	"len":   object.GetBuiltinByName("len"),
+	"first": object.GetBuiltinByName("first"),
+	"last":  object.GetBuiltinByName("last"),
+	"rest":  object.GetBuiltinByName("rest"),
+	"push":  object.GetBuiltinByName("push"),
+	"puts":  object.GetBuiltinByName("puts"),
 }
