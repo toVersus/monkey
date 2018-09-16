3c4f83c2846a024c934f7a3b64e0dbf5c8f452db
toversus <toversus2357@gmail.com>
2018-09-16 09:23:51 +0900

    support function literals in modification

--- ast/modify.go
+++ ast/modify.go
@@ -40,6 +40,12 @@ func Modify(node Node, modifier ModifyFunc) Node {
 
 	case *LetStatement:
 		node.Value, _ = Modify(node.Value, modifier).(Expression)
+
+	case *FunctionLiteral:
+		for i := range node.Parameters {
+			node.Parameters[i], _ = Modify(node.Parameters[i], modifier).(*Identifier)
+		}
+		node.Body, _ = Modify(node.Body, modifier).(*BlockStatement)
 	}
 
 	return modifier(node)
